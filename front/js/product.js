b4f742d41695fae5135f3797922cf2eb60ceaaf3
Claire Royer <claireroyer57@gmail.com>
2021-11-10 14:55:18 +0100

    Add error systeme + add details of products on product page

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,27 @@
-let str = window.location.href;
-let url = new URL(str);
-let id = url.searchParams.get("id");
+const loc = window.location.href;
+const url = new URL(loc);
+const id = url.searchParams.get("id");
 console.log(id);
+
+fetch(`http://localhost:3000/api/products/${id}`)
+  .then((response) => {
+    if (response.ok) return response.json();
+    throw new Error("mauvais id");
+  })
+  .then((product) => {
+    console.log(product);
+
+    price.innerHTML = product.price;
+
+    description.innerHTML = product.description;
+
+    const productColors = product.colors;
+    console.log(productColors);
+
+    for (let i = 0; i < productColors.length; i++) {
+      const optionElement = document.createElement("option");
+      colors.appendChild(optionElement);
+      optionElement.setAttribute("value", productColors[i]);
+      optionElement.innerHTML = productColors[i];
+    }
+  });
