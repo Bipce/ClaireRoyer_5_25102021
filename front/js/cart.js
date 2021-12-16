0c68b8ed3383e8ba15557d85514a8fe8a513f5a8
Claire Royer <claireroyer57@gmail.com>
2021-12-16 14:52:21 +0100

    Create fonction for updateProduct + calcul the total price

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,13 +5,13 @@ const setView = () => {
   itemsCart.innerHTML = "";
   for (let i = 0; i < products.length; i++) {
     itemsCart.innerHTML += `
-  <article class="cart__item" data-id="${products[i]._id}">
+  <article class="cart__item" data-id="${products[i]._id}" data-color="${products[i].color}">
                 <div class="cart__item__img">
                   <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
                 </div>
                 <div class="cart__item__content">
                   <div class="cart__item__content__titlePrice">
-                    <h2>${products[i].name}</h2>
+                    <h2>${products[i].name} ${products[i].color}</h2>
                     <p>${products[i].price} €</p>
                   </div>
                   <div class="cart__item__content__settings">
@@ -35,25 +35,66 @@ const deleteCarts = document.getElementsByClassName("deleteItem");
 console.log(deleteCarts);
 const key = "products";
 
-const deleteProduct = (id) => {
+const deleteProduct = (id, color) => {
   for (let i = 0; i < products.length; i++) {
-    if (products[i]._id === id) {
+    if (products[i]._id === id && products[i].color === color) {
       products.splice(i, 1);
       break;
     }
   }
 
   // Other way to do it.
-  // const index = products.findIndex((p) => p._id === id);
+  // const index = products.findIndex((p) => p._id === id && p.color === color);
   // products.splice(index, 1);
 
+  displayTotal();
   localStorage.setItem(key, JSON.stringify(products));
 };
 
 for (let i = 0; i < deleteCarts.length; i++) {
   deleteCarts[i].addEventListener("click", (e) => {
     const article = e.target.closest("article");
-    deleteProduct(article.getAttribute("data-id"));
+    deleteProduct(article.dataset.id, article.dataset.color);
     article.remove();
   });
 }
+
+const updateProduct = (id, color, quantity) => {
+  for (let i = 0; i < products.length; i++) {
+    if (products[i]._id === id && products[i].color === color) {
+      products[i].quantity = quantity;
+      break;
+    }
+  }
+
+  localStorage.setItem(key, JSON.stringify(products));
+  displayTotal();
+};
+
+const changeProduct = document.getElementsByClassName("itemQuantity");
+
+for (let i = 0; i < changeProduct.length; i++) {
+  changeProduct[i].addEventListener("change", (e) => {
+    const quantity = parseInt(changeProduct[i].value);
+
+    const article = e.target.closest("article");
+    updateProduct(article.dataset.id, article.dataset.color, quantity);
+  });
+}
+
+const displayTotal = () => {
+  let totalPrice = 0;
+  let totalQuantity = 0;
+
+  for (let i = 0; i < products.length; i++) {
+    totalQuantity += products[i].quantity;
+    totalPrice += products[i].price * products[i].quantity;
+  }
+  const price = document.getElementById("totalPrice");
+  const quantity = document.getElementById("totalQuantity");
+
+  price.innerHTML = totalPrice;
+  quantity.innerHTML = totalQuantity;
+};
+
+displayTotal();
